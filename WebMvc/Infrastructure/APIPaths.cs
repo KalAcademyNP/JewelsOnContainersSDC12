3a3187520359d09dc575e9e48d06c903640b6288
Kal <info@kalacademy.org>
2022-04-09 12:18:01 -0700

    Cart integration with webmvc

--- WebMvc/Infrastructure/APIPaths.cs
+++ WebMvc/Infrastructure/APIPaths.cs
@@ -43,5 +43,23 @@ namespace WebMvc.Infrastructure
                 return preUri;
             }
         }
+
+        public static class Basket
+        {
+            public static string GetBasket(string baseUri, string basketId)
+            {
+                return $"{baseUri}/{basketId}";
+            }
+
+            public static string UpdateBasket(string baseUri)
+            {
+                return baseUri;
+            }
+
+            public static string CleanBasket(string baseUri, string basketId)
+            {
+                return $"{baseUri}/{basketId}";
+            }
+        }
     }
 }
